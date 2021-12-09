41479de060e602241fd019a75f8a9418b15782ea
IvinChan <475477428@qq.com>
2021-12-09 19:54:13 +0800

    Add files via upload

--- Verilog/NPU_CUBE_ADD_TEST.sv
+++ Verilog/NPU_CUBE_ADD_TEST.sv
@@ -1,8 +1,8 @@
 `timescale 1ns / 1ps
 module NPU_CUBE_ADD_TEST;
 	reg clk;
-	wire [18:0] result_hard_sign;
-	wire [18:0] result_hard_unsign;
+	reg [18:0] result_hard_sign;
+	reg [18:0] result_hard_unsign;
 	wire [18:0] result_soft_unsign;
 	wire [18:0] result_soft_sign;
     reg [63:0]  mul1;
@@ -14,7 +14,7 @@ module NPU_CUBE_ADD_TEST;
 	initial begin
 		clk = 0;
 		forever
-		#50 clk = ~clk;
+		#1 clk = ~clk;
 	end
 
     initial
@@ -35,9 +35,9 @@ module NPU_CUBE_ADD_TEST;
 
     initial begin
     `ifdef SDF_PR_SIM
-    $sdf_annotate("/NPU_ADD_TREE/PR/data/NPU_ADD_TREE_TOP.sdf",
+    $sdf_annotate("/home/chengquan/DC_project/NPU_ADD_TREE/PR/data/NPU_ADD_TREE_TOP.sdf",
     NPU_CUBE_ADD_TOP_sign,,,
-    "TYPICAL",
+    "MAXIMUM",
     "1:1:1",
     "FROM_MTM");
     `endif
@@ -56,7 +56,7 @@ module NPU_CUBE_ADD_TEST;
 			mul1[63:32] <= $random(seed);
 			mul2[63:0] <= $random(seed);//
 
-			#100
+			#2
 			if(result_soft_unsign!=result_hard_unsign)
 			begin
 				flag=0;
@@ -81,7 +81,7 @@ module NPU_CUBE_ADD_TEST;
 	end	
 
 
-assign result_soft_unsign =  $signed({1'b0,mul1[ 7: 0]})*$signed(mul2[ 7: 0]) +
+assign #2 result_soft_unsign =  $signed({1'b0,mul1[ 7: 0]})*$signed(mul2[ 7: 0]) +
 							$signed({1'b0,mul1[15: 8]})*$signed(mul2[15: 8]) + 
 							$signed({1'b0,mul1[23:16]})*$signed(mul2[23:16]) + 
 							$signed({1'b0,mul1[31:24]})*$signed(mul2[31:24]) + 
@@ -90,7 +90,7 @@ assign result_soft_unsign =  $signed({1'b0,mul1[ 7: 0]})*$signed(mul2[ 7: 0]) +
 							$signed({1'b0,mul1[55:48]})*$signed(mul2[55:48]) + 
 							$signed({1'b0,mul1[63:56]})*$signed(mul2[63:56]) ; 
 
-assign result_soft_sign = $signed({mul1[ 7: 0]})*$signed(mul2[ 7: 0]) +
+assign #2 result_soft_sign = $signed({mul1[ 7: 0]})*$signed(mul2[ 7: 0]) +
 					     $signed({mul1[15: 8]})*$signed(mul2[15: 8]) + 
 						 $signed({mul1[23:16]})*$signed(mul2[23:16]) + 
 					     $signed({mul1[31:24]})*$signed(mul2[31:24]) + 
@@ -99,6 +99,16 @@ assign result_soft_sign = $signed({mul1[ 7: 0]})*$signed(mul2[ 7: 0]) +
 						 $signed({mul1[55:48]})*$signed(mul2[55:48]) + 
 						 $signed({mul1[63:56]})*$signed(mul2[63:56]) ; 
 
+                         
+wire [18:0] result_hard_sign_s;
+wire [18:0] result_hard_unsign_s;
+
+
+always @(posedge clk)
+    begin
+        result_hard_sign   <= result_hard_sign_s;
+        result_hard_unsign <= result_hard_unsign_s;
+    end
 
 
 	NPU_ADD_TREE_TOP NPU_CUBE_ADD_TOP_sign(
@@ -106,7 +116,7 @@ assign result_soft_sign = $signed({mul1[ 7: 0]})*$signed(mul2[ 7: 0]) +
     .add_tree_data(mul1),
     .add_tree_para(mul2),
     .is_signed_data(1'b1),
-    .add_result(result_hard_sign)
+    .add_result(result_hard_sign_s)
 	);
 
 	NPU_ADD_TREE_TOP NPU_CUBE_ADD_TOP_unsign(
@@ -114,7 +124,7 @@ assign result_soft_sign = $signed({mul1[ 7: 0]})*$signed(mul2[ 7: 0]) +
     .add_tree_data(mul1),
     .add_tree_para(mul2),
     .is_signed_data(1'b0),
-    .add_result(result_hard_unsign)
+    .add_result(result_hard_unsign_s)
 	);
 
 
